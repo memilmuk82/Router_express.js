aa2249ec453103353eaae74a7037d4adaeb9fc87
JinseonLee <memilmuk82@gmail.com>
2024-04-17 08:52:03 +0900

    에러 html 수정중

--- app.js
+++ app.js
@@ -72,17 +72,28 @@ app.use('/', indexRouter);
 // '/user' 경로에 대한 라우터 등록
 app.use('/user', userRouter);
 
-// 위에서 등록된 라우터에 일치하는 경로가 없는 경우 -> 404 에러 처리
+// 위에서 등록된 라우터에 일치하는 경로가 없는 경우
 app.use((req, res, next) => {
-    res.status(404).send('Not Found');  
+    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다`)
+    // 새로운 에러 객체를 생성 -> 에러 메시지로 현재 요청의 메서드와 URL을 설정
+    error.status = 404;
+    // 에러 객체의 상태 코드를 404로 설정
+    next(error);  
+    // 다음 미들웨어에게 에러를 전달
 });
 
-// 에러 핸들링 미들웨어 -> 에러가 발생한 경우 500 에러 응답
+// 에러 핸들링 미들웨어 -> 에러가 발생한 경우 실행
 app.use((err, req, res, next) => {
-    console.error(err);
-    // 에러 로그 출력
-    res.status(500).send(err.message);
-    // 에러 메시지를 응답으로 전송
+    res.locals.message = err.message;
+    // 에러 메시지를 로컬 변수에 할당
+    res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+    // process.env.NODE_ENV -> 현재 실행 환경을 나타내는 Node.js의 환경 변수
+    // 에러 객체를 'production'(배포) 환경이 아닌 경우(개발환경 또는 테스트 환경)에만 로컬 변수에 할당
+    // 'production'(배표) 환경에서는 빈 객체 할당 -> 보안 및 개인정보 강화(error.html의 error.stack 노출 차단)
+    res.status(err.status || 500);
+    // 응답 상태 코드를 에러 객체의 상태 코드 또는 500으로 설정
+    res.render('error');
+    // 'error' 템플릿을 렌더링하여 에러 페이지를 표시
 });
 
 // 지정된 포트에서 서버 대기
